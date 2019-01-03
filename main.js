407a83c971b0614799157da978fbefe9cd009760
Divya Samp <radsin99@gmail.com>
2019-01-03 11:42:17 +0530

    Toggle menu

---
+++ main.js
@@ -0,0 +1,45 @@
+
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+
+    let getSidebar = document.querySelector(".nav-sidebar");
+    let getSidebarUl = document.querySelector(".nav-sidebar ul");
+    let getSidebarSpan = document.querySelector(".nav-sidebar span");
+    let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+
+
+    if (toggleNavStatus === false) {
+
+        getSidebarUl.style.visibility = "visible";
+        getSidebar.style.width = "270px";
+        getSidebarSpan.style.opacity = "0.5";
+
+
+        let arrayLength = getSidebarLinks.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getSidebarLinks[i].style.opacity = "1";
+        }
+
+        toggleNavStatus = true;
+    }
+
+
+   
+   else if (toggleNavStatus === true) {
+
+        getSidebarUl.style.visibility = "hidden";
+        getSidebar.style.width = "50px";
+        getSidebarSpan.style.opacity = "0";
+
+
+        let arrayLength = getSidebarLinks.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getSidebarLinks[i].style.opacity = "0";
+        }
+
+        toggleNavStatus = false;
+    } 
+}
+
